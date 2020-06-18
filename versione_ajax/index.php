28586cd820e547b8cdf52509a3bd8b11d5340a02
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-06-18 17:09:22 +0200

    push intermedia

---
+++ versione_ajax/index.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <link rel="stylesheet" href="../public/style.css">
+         <title>Dischi musicali</title>
+     </head>
+     <body>
+
+
+
+
+
+
+
+     <script src="../public/main.js" charset="utf-8"></script>
+     </body>
+ </html>
